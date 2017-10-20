f1e4f461b1f1e467809c826900f9a5e1a2929012
stallmd <31293406+stallmd@users.noreply.github.com>
2017-10-20 10:58:57 -0400

    checkName

---
+++ lab6.js
@@ -0,0 +1,34 @@
+function checkName(){
+    var regExp = new RegExp('<');
+    if(regExp.test(document.getElementById('name').value)){
+        document.getElementById('name').style.backgroundColor='red';
+    } else{
+        document.getElementById('name').style.backgroundColor='';
+    }
+}
+
+
+
+
+
+
+function checkAge(age){
+
+}
+
+
+
+
+
+
+
+function checkEmail(email){
+    var read =
+}
+
+
+function setBackground(){
+
+}
+
+
